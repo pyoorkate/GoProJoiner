dca5917cc5e2935378a516c7b020ff565912004f
pyoorkate <88565252+pyoorkate@users.noreply.github.com>
2023-04-19 18:00:23 -0700

    Update GoProJoin.py
    Okay, so there were some errors in the FFMPEG subprocess strings which I propagated through when I was trying to be clever. This is now fixed, along with an allowance for custom names which is currently 'bodgy'.

--- GoProJoin.py
+++ GoProJoin.py
@@ -2,19 +2,51 @@ import os
 import re
 import subprocess
 
-print("Very basic GoPro File Sort-and-Concatinator\n")
+print("Less and less basic GoPro File Sort-and-Concatinator")
+print("====================================================")
 # Written using ChatGPT, a lot of fiddling to get it to kinda work, then me polishing the turd
 print("Operates in current working directory & assumes FFMPEG is available in your path\n\n")
 
 video_files = os.listdir('.')
 
-primary_segments = sorted([x for x in video_files if re.search(r'^GOPR\d+\.MP4$', x)])
-secondary_segments = sorted([x for x in video_files if re.search(r'^GP\d+\.MP4$', x)])
+# Do they use a custom file naming convention (Like Nikki does, gods damn it)
+custom_naming = ""
+print("Do you use custom or default gopro filenames")
+print("============================================\n")
+print("At the moment, custom just assumes that anything before \nGOPR or GP is the custom name. \nIt will match anything with GOPR or GP in the name")
+print("There's a very faint possibility that I might fix this at some point.")
+print("\n(D)efault")
+print("(C)ustom\n")
+custom_naming = input("Custom or Default: ")
+while custom_naming != "D" and custom_naming != "C":
+	custom_naming = input("Please choose D or C: ")
+	
+if custom_naming == "D":
+	primary_pattern = r'^GOPR\d+\.MP4$'
+	primary_pattern_writing = r'^GOPR(\d+)\.MP4$'
+	secondary_pattern = r'^GP\d+\.MP4$'
+	secondary_pattern_writing = r'^GP\d+(\d{4})\.MP4$'
+	
+if custom_naming == "C":
+# At present custom naming is just playing fast and loose.
+	primary_pattern = r'^.*GOPR\d+\.MP4$'
+	primary_pattern_writing = r'^.*GOPR(\d+)\.MP4$'	
+	secondary_pattern = r'^.*GP\d+\.MP4$'
+	secondary_pattern_writing = r'^.*GP\d+(\d{4})\.MP4$'
+
+# Match those files
+primary_segments = sorted([x for x in video_files if re.search(primary_pattern, x)])
+secondary_segments = sorted([x for x in video_files if re.search(secondary_pattern, x)])
+
+
+# Basic matching	
+# primary_segments = sorted([x for x in video_files if re.search(r'^GOPR\d+\.MP4$', x)])
+# secondary_segments = sorted([x for x in video_files if re.search(r'^GP\d+\.MP4$', x)])
 
 # Identify whether the user wants all files combined together, or each camera start
 # as a separate file
 allasone = ""
-print("Choose output type")
+print("\nChoose output type")
 print("==================\n")
 print("(A)ll as one file")
 print("(I)ndividual files for each camera start\n")
@@ -26,7 +58,7 @@ while allasone != "A" and allasone != "I":
 
 # Do they want to output to a different directory (just here because my friend's script was posher than mine)
 # Get an output directory, if this is blank use the current working directory
-output_directory = input("Output file directory (leave blank for current working directory): ")
+output_directory = input("\nOutput file directory (leave blank for current working directory): ")
 if output_directory == "":
 	output_directory = os.getcwd()
 
@@ -48,7 +80,7 @@ if output_directory_exists == False:
 
 
 if allasone == "A":
-	print("The following files and segments have been located:/n")
+	print("The following files and segments have been located:\n")
 	# Create a new file to write the sorted filenames to
 	with open("sorted_files.txt", "w") as f:
 		for i, primary_file in enumerate(primary_segments):
@@ -56,8 +88,8 @@ if allasone == "A":
 			print(primary_file)
 			# Write the primary segment file name to the output file
 			f.write("file\t" + primary_file + "\n")
-			primary_segment_num = int(re.search(r'^GOPR(\d+)\.MP4$', primary_file).group(1))
-			matching_secondary_segments = sorted([x for x in secondary_segments if primary_segment_num == int(re.search(r'^GP\d+(\d{4})\.MP4$', x).group(1)[:4])])
+			primary_segment_num = int(re.search(primary_pattern_writing, primary_file).group(1))
+			matching_secondary_segments = sorted([x for x in secondary_segments if primary_segment_num == int(re.search(secondary_pattern_writing, x).group(1)[:4])])
 			print("Secondary segments")
 			for secondary_file in matching_secondary_segments:
 				print(secondary_file)
@@ -69,7 +101,7 @@ if allasone == "A":
 	# join together yon filename and the path input earlier
 	output_full_filename = os.path.join(output_directory, outputname)
 	# Call FFMPEG using the full filename and directory for output
-	subprocess.call(['ffmpeg', '-y', '-f', 'concat', '-i', output_full_filename, '-c', 'copy', '-map', '0:0', '-map', '0:1', '-map', '0:3', outputname ])
+	subprocess.call(['ffmpeg', '-y', '-f', 'concat', '-i', 'sorted_files.txt', '-c', 'copy', '-map', '0:0', '-map', '0:1', '-map', '0:3', output_full_filename ])
 	print(f"Hopefully you have a file called {outputname} in {output_directory}. It should have all the files combined in one.")
 	exit()
 
@@ -85,8 +117,8 @@ if allasone == "I":
 		with open("sorted_file%s.txt" % i, "w") as f:
 			# Write the primary segment file name to the output file
 			f.write("file\t" + primary_file + "\n")
-			primary_segment_num = int(re.search(r'^GOPR(\d+)\.MP4$', primary_file).group(1))
-			matching_secondary_segments = sorted([x for x in secondary_segments if primary_segment_num == int(re.search(r'^GP\d+(\d{4})\.MP4$', x).group(1)[:4])])
+			primary_segment_num = int(re.search(primary_pattern_writing, primary_file).group(1))
+			matching_secondary_segments = sorted([x for x in secondary_segments if primary_segment_num == int(re.search(secondary_pattern_writing, x).group(1)[:4])])
 			print("Secondary segments")
 			for secondary_file in matching_secondary_segments:
 				print(secondary_file)
@@ -102,12 +134,13 @@ if allasone == "I":
 	segmentcount = segmentcount - 1
 	while segmentcount > -1:
 		# Munge together a filename containing the current segment and the all the other bits of the name
-		current_segment_file = "sorted_files" + str(segmentcount) + ".txt"
+		current_segment_file = "sorted_file" + str(segmentcount) + ".txt"
 		# join together yon filename and the path input earlier
-		output_full_filename = os.path.join(output_directory, current_segment_file)
+		segment_count_stringified = str(segmentcount)
+		output_full_filename = os.path.join(output_directory, outputname, segment_count_stringified + ".MP4")
 		print(f"Launching FFMPEG for " + output_full_filename )
 		# Call FFMPEG using the sorted_files filename as a source and outputting to outputname
-		subprocess.call(['ffmpeg', '-y', '-f', 'concat', '-i', output_full_filename, '-c', 'copy', '-map', '0:0', '-map', '0:1', '-map', '0:3', outputname ])
+		subprocess.call(['ffmpeg', '-y', '-f', 'concat', '-i', current_segment_file, '-c', 'copy', '-map', '0:0', '-map', '0:1', '-map', '0:3', output_full_filename ])
 		segmentcount = segmentcount - 1
 		
 		
@@ -115,6 +148,6 @@ if allasone == "I":
 	exit()
 	
 else:
-	print("Uncaught error (I mean, I did tons of error checking, so this is totally unexpected... ;) )")
+	print("Uncaught error (I mean, and I swear I did tons of error checking, so this is totally unexpected... ;) )")
 	exit()
 	
